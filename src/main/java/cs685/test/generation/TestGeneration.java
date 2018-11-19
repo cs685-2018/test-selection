f62bee48cae1deba34c8ac928c02c855eaf072b1
Ryan Zembrodt <ryan.zembrodt@uky.edu>
2018-11-19 01:37:29 -0500

    Possible github repository fix

--- src/main/java/cs685/test/generation/TestGeneration.java
+++ src/main/java/cs685/test/generation/TestGeneration.java
@@ -1,6 +1,8 @@
 package cs685.test.generation;
 
 import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
@@ -22,10 +24,19 @@ public class TestGeneration {
     
     public TestGeneration(HashMap<String, List<String>> map) {
         this.map = map;
-        
-    	File remoteFile = new File("https://github.com/zembrodt/cs685-hw2"); // TODO: get git repo url from jenkins project
+        /*
+        URI remoteURL = null;
+		try {
+			remoteURL = new URI("https:///github.com/zembrodt/cs685-hw2");
+		} catch (URISyntaxException e) {
+			System.out.println("Could not find github url");
+			e.printStackTrace();
+		}
+    	File remoteFile = new File(remoteURL); // TODO: get git repo url from jenkins project
     	FilePath workspaceTargetDir = new FilePath(remoteFile);
-    	this.gitRepository = GitLastChanges.repository(workspaceTargetDir.getRemote() + "/.git");
+    	*/
+    	//this.gitRepository = GitLastChanges.repository(workspaceTargetDir.getRemote() + "/.git");
+    	this.gitRepository = GitLastChanges.repository("https://github.com/zembrodt/cs685-hw2/.git");
     	this.lastChanges = GitLastChanges.getInstance().changesOf(gitRepository);
     }
 
